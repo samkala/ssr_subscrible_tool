b941e2b4de61cdcf7ce820e5c1e0c853975aa449
samkala <monkey.d.samkala@gmail.com>
2019-05-01 11:02:46 +0800

    zxx

--- app.js
+++ app.js
@@ -15,13 +15,10 @@ let BUILD_DIR = 'dist' // 构建目录
 let ENTRY_FILE = './node.txt' // SS(R)入口
 let str = readSync(ENTRY_FILE)
 
-let checker = item => {
-    return item => item.includes('ssr://') || item.includes('ss"//')
-}
 
 // Map all the item include `ssr://` and serialize those items
-let result = str.split('\n\n')
-                .filter(item => checker(item))
+let result = str.split(/\s+/)
+                .filter(item => item.includes('ssr://') || item.includes('ss"//'))
                 .join('\r\n')
 
 writeSync(path.resolve(__dirname, BUILD_DIR, 'index.html'), base64(result))
